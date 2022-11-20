0918ed39378788b3307d1d25deea461529d9cdde
Maria José Solano <majosolano99@gmail.com>
2022-11-20 09:37:24 -0800

    Replace demo with tests

--- rust-book-exercises/src/collections.rs
+++ rust-book-exercises/src/collections.rs
@@ -1,10 +1,12 @@
+#![allow(dead_code)]
+
 use regex::Regex;
 use std::{collections::HashMap, io};
 
 // Given a list of integers, use a vector and return the median (when sorted,
 // the value in the middle position) and mode (the value that occurs most often; a hash
 // map will be helpful here) of the list.
-pub fn median(list: &Vec<i32>) -> f32 {
+fn median(list: &Vec<i32>) -> f32 {
     let mut copy = list.clone();
     copy.sort();
 
@@ -16,7 +18,7 @@ pub fn median(list: &Vec<i32>) -> f32 {
     }
 }
 
-pub fn mode(list: &Vec<i32>) -> Vec<i32> {
+fn mode(list: &Vec<i32>) -> Vec<i32> {
     if list.is_empty() {
         return vec![];
     }
@@ -44,7 +46,7 @@ pub fn mode(list: &Vec<i32>) -> Vec<i32> {
 // end of the word and “ay” is added, so “first” becomes “irst-fay.”
 // Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”).
 // Keep in mind the details about UTF-8 encoding!
-pub fn convert_to_pig_latin(s: &mut String) -> () {
+fn convert_to_pig_latin(s: &mut String) {
     if s.is_empty() {
         return;
     }
@@ -64,10 +66,11 @@ pub fn convert_to_pig_latin(s: &mut String) -> () {
 // employee names to a department in a company. For example, “Add Sally to Engineering” or
 // “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or
 // all people in the company by department, sorted alphabetically.
-pub fn company_app() {
+fn company_app() {
     let mut department_members: HashMap<String, Vec<String>> = HashMap::new();
     let add_re = Regex::new(r"^Add ([[:alpha:]]+) to ([[:alpha:]]+)").unwrap();
     let list_re = Regex::new(r"^List ([[:alpha:]]+)").unwrap();
+
     loop {
         println!("\nWhat do you want to do?");
 
@@ -105,5 +108,44 @@ pub fn company_app() {
         }
 
         break;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn median_odd_length_array() {
+        assert_eq!(0.0, median(&vec![1, 0, -1]));
+    }
+
+    #[test]
+    fn median_even_length_array() {
+        assert_eq!(1.5, median(&vec![1, 2, 0, 3]));
+    }
+
+    #[test]
+    fn single_mode() {
+        assert_eq!(vec![1], mode(&vec![1, 1, 0]));
+    }
+
+    #[test]
+    fn multiple_modes() {
+        assert_eq!(vec![0, 1], mode(&vec![1, 1, 0, 0]));
+    }
+
+    #[test]
+    fn pig_latin_starts_with_consonant() {
+        let mut word = String::from("first");
+        convert_to_pig_latin(&mut word);
+        assert_eq!(String::from("irst-fay"), word);
+    }
+
+    #[test]
+    fn pig_latin_starts_with_vowel() {
+        let mut word = String::from("apple");
+        convert_to_pig_latin(&mut word);
+        assert_eq!(String::from("apple-hay"), word);
     }
 }
